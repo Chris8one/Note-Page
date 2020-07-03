a42648aeb865e2f32305b8ccbb33b66f23b2eb09
Christer Granbäck <crille_granback@hotmail.com>
2020-07-03 12:48:50 +0000

    Made all pages to work as intended

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -18,11 +19,31 @@ def get_notes():
 def add_note():
     return render_template("addnote.html")
 
-@app.route('/insert_note')
+@app.route('/insert_note', methods=['POST'])
 def insert_note():
     notes = mongo.db.notes
     notes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_notes'))
+    return redirect(url_for('get_notes', _anchor="notes"))
+
+@app.route('/edit_note/<note_id>')
+def edit_note(note_id):
+    the_note = mongo.db.notes.find_one({"_id": ObjectId(note_id)})
+    return render_template("editnote.html", note=the_note)
+
+@app.route('/update_note/<note_id>', methods=['POST'])
+def update_note(note_id):
+    notes = mongo.db.notes
+    notes.update({'_id': ObjectId(note_id)},
+    {
+        'note_title': request.form.get('note_title'),
+        'note_information': request.form.get('note_information')
+    })
+    return redirect(url_for('get_notes', _anchor="notes"))
+
+@app.route('/delete_note/<note_id>')
+def delete_note(note_id):
+    mongo.db.notes.remove({"_id": ObjectId(note_id)})
+    return redirect(url_for('get_notes', _anchor="notes"))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
